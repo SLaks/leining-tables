11baf1acad6e5d8f77b851cb44d2562ec11c0255
SLaks <Dev@SLaks.net>
2025-04-16 18:14:47 -0400

    Tables: Add Holiday summary to Parsha titles

--- src/table-generator/table-data.ts
+++ src/table-generator/table-data.ts
@@ -2,6 +2,7 @@ import { Locale } from "@hebcal/leyning/dist/esm/locale";
 import { LeiningInfo } from "./get-leinings";
 import { Aliyah, AliyotMap } from "@hebcal/leyning";
 import { getSefer, toHebrewDisplay } from "../names";
+import { flags } from "@hebcal/core/dist/esm/event";
 
 export type LeiningTableRow = ReturnType<typeof generateRows>[number];
 
@@ -11,8 +12,29 @@ export function generateRows(
 ) {
   return leinings.map((o) => {
     let title = Locale.hebrewStripNikkud(o.name.he ?? "TODO: unknown");
-    if (o.parsha) title = `פרשת ${title}`;
-
+    if (o.parsha) {
+      title = `פרשת ${title}`;
+      // There is no HolidayEvent for מחר חודש, so we add it manually.
+      // It does appear in .reason strings, but those are too verbose.
+      const extraSpecial = [];
+      if (o.date.getDate() === 29) extraSpecial.push("מחר חודש");
+      const special =
+        o.holidays
+          ?.filter(
+            (h) =>
+              h.mask &
+              (flags.SPECIAL_SHABBAT |
+                flags.ROSH_CHODESH |
+                flags.CHANUKAH_CANDLES)
+          )
+          // Strip the day of חנוכה.
+          .map((h) =>
+            h.renderBrief("he-x-nonikud").replace(/חנוכה.*/, "שבת חנוכה")
+          )
+          .concat(extraSpecial)
+          .join(", ") ?? "";
+      if (special) title += ` (${special})`;
+    }
     const haftara = [(sephardic && o.seph) || o.haft].flat();
 
     return {
@@ -23,6 +45,7 @@ export function generateRows(
 
       ...mainInfo(),
       ...haftaraInfo(),
+      // TODO: מגילה
     };
 
     function mainInfo() {
