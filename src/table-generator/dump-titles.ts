abc73897007b8209fe77c92cd1b67a0d96f767d7
SLaks <Dev@SLaks.net>
2025-04-16 18:52:40 -0400

    Dump all possible titles

--- src/table-generator/dump-titles.ts
+++ src/table-generator/dump-titles.ts
@@ -1,23 +1,28 @@
 import { stdout } from "process";
 import { getLeinings } from "./get-leinings";
 import { generateRows } from "./table-data";
+import { getYearTypes } from "../logic/year-types";
 
 stdout.setEncoding("utf8");
 
 stdout.write(
   JSON.stringify(
-    generateRows(
-      getLeinings([5785], {
-        includeParshiyos: true,
-        includeYomTov: true,
-        includeCholHamoed: false,
-        includeFastDays: false,
-        israeli: false,
-      }),
-      { sephardic: false }
-    )
-      .filter((o) => o.title.includes("("))
-      .map((o) => o.title),
+    [
+      ...new Set(
+        generateRows(
+          getLeinings([...getYearTypes().values()], {
+            includeParshiyos: true,
+            includeYomTov: true,
+            includeCholHamoed: false,
+            includeFastDays: false,
+            israeli: false,
+          }),
+          { sephardic: false }
+        )
+          .filter((o) => o.title.includes("("))
+          .map((o) => o.title)
+      ),
+    ],
     null,
     2
   )
